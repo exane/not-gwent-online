c468147c90a944be6296c74b09582609dce1499e
devfake <devfakeplus@googlemail.com>
2015-06-22 20:21:31 +0200

    minor

--- site/client/app/components/chat.js
+++ site/client/app/components/chat.js
@@ -1,5 +1,19 @@
 module.exports = {
 
-  template: require('../views/chat.html')
+  template: require('../views/chat.html'),
+
+  data: function() {
+    return {
+      message: ''
+    }
+  },
+
+  methods: {
+    submitChat: function(e) {
+      e.preventDefault();
+
+      console.log(this.message);
+    }
+  }
 
 };
