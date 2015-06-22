9654ba6a50748a5f8db6b837ba66a7116e76c7ee
devfake <devfakeplus@googlemail.com>
2015-06-22 18:12:34 +0200

    add routing

--- site/client/app/routes.js
+++ site/client/app/routes.js
@@ -1,26 +1,19 @@
-var routes = {
-  '/lobby': function() {
-    alert('lobby');
-  },
-
-  '/deck-builder': function() {
-    alert('deck-builder');
-  },
-
-  '/highscore': function() {
-    alert('highscore');
-  }
-};
-
-var options = {
-  html5history: true
-}
-
-var Router = require('director').Router;
-var router = new Router(routes).configure(options).init();
+require('../../../public/Config.js');
 
 module.exports = {
-  changeRoute: function(route) {
-    router.setRoute(route);
+
+  maps: {
+    '/lobby': {
+      component: 'lobby'
+    },
+
+    '/deck-builder': {
+      component: 'deckBuilder'
+    }
+  },
+
+  options: {
+    history: true,
+    root: window.Config.Site.base
   }
 }
