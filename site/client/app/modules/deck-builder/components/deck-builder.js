ddd168b2dd6eebda276898cc0bdb3d250d75d436
devfake <devfakeplus@googlemail.com>
2015-06-24 07:09:16 +0200

    few simple changes

--- site/client/app/modules/deck-builder/components/deck-builder.js
+++ site/client/app/modules/deck-builder/components/deck-builder.js
@@ -1,5 +1,35 @@
+var cards = require("../../../../../../assets/data/cards");
+var deck = require("../../../../../../assets/data/deck");
+
 module.exports = {
 
-  template: require('../views/deck-builder.html')
+  template: require('../views/deck-builder.html'),
 
+  data: function() {
+    return {
+      cards: [],
+      deck: []
+    }
+  },
+
+  ready: function() {
+    this.cards = $.map(cards, function(n) {
+      if(n.faction == 'Northern Realm' && n.type != 3) return n;
+    });
+
+    $.map(deck, (n, i) => {
+      if(i == 'northern_realm') {
+        for(var m in n) {
+          //console.log(this.cards[m].type);
+          this.deck.push(this.cards[m]);
+        }
+      }
+    });
+  },
+
+  filters: {
+    leader: function(deck) {
+      //return deck.type == 3 ? deck : 'ne';
+    }
+  }
 };
--- site/server/config/database.php
+++ site/server/config/database.php
@@ -58,6 +58,7 @@ return [
             'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
             'password'  => env('DB_PASSWORD', ''),
+            'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
