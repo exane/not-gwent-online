b9f9141caf3d92b89e50304054a2682d7fa1b7a6
exane <raco0n@gmx.de>
2015-06-27 13:23:05 +0200

    relocate images + include sprite generator

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,15 @@ var browserify = require('browserify');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var fs = require("fs");
+var spritesmith = require("gulp.spritesmith");
 var babelify = require("babelify");
 var livereload = require("gulp-livereload");
 var sass = require("gulp-sass");
 var handlebars = require("browserify-handlebars");
+var imagemin = require('gulp-imagemin');
+var gm = require("gulp-gm");
+var sprity = require("sprity");
+var gulpif = require("gulp-if");
 livereload({start: true});
 
 //fast install
@@ -67,7 +72,9 @@ gulp.task("watch", function(){
   gulp.watch("./test/src/*", ["unit tests"]);
 })
 
-gulp.task("index", function() {
+
+
+gulp.task("index", function(){
   gulp.src("./client/index.html")
   .pipe(gulp.dest("./public/"));
 
@@ -75,4 +82,44 @@ gulp.task("index", function() {
   .pipe(gulp.dest("./public/build"));
 })
 
-gulp.task("default", ["watch", "browserify", "sass", "unit tests", "index"]);
+gulp.task('resize', function(){
+  if(fs.existsSync(__dirname + "/assets/cards/lg/monster/arachas1.png")) {
+    console.log("skip resizing");
+    return;
+  }
+  gulp.src('./assets/original_cards/**/*.png')
+  .pipe(gm(function(gmfile){
+    return gmfile.resize(null, 120);
+  }))
+  .pipe(gulp.dest('./assets/cards/sm/'));
+
+  gulp.src('./assets/original_cards/**/*.png')
+  .pipe(gm(function(gmfile){
+    return gmfile.resize(null, 450);
+  }))
+  .pipe(gulp.dest('./assets/cards/lg/'));
+});
+
+gulp.task("sprite", function(){
+  /*if(fs.existsSync(__dirname + "/public/build/")) {
+    console.log("skip resizing");
+    return;
+  }*/
+  sprity.src({
+    src: "./assets/cards/**/*.png",
+    style: "_cards.scss",
+    //"style-type": "scss",
+    processor: "css",
+    engine: "gm",
+    orientation: "binary-tree",
+    split: true,
+    cssPath: "../../public/build/",
+    prefix: "card",
+    name: "cards",
+    margin: 0
+    //template: "./client/scss/_cards.hbs"
+  })
+  .pipe(gulpif("*.png", gulp.dest("./public/build/"), gulp.dest("./client/scss/")))
+})
+
+gulp.task("default", ["watch", "browserify", "sass", "unit tests", "index", "resize", "sprite"]);
