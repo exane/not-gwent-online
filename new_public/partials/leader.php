676e40f37bb78824c3ffd29a2d1ed5910c7242f6
devfake <devfakeplus@googlemail.com>
2015-07-02 21:52:40 +0200

    push this...

---
+++ new_public/partials/leader.php
@@ -0,0 +1,16 @@
+<div class="leader-wrap">
+
+  <div class="leader-card-wrap">
+
+    <div class="leader-card">
+      <div class="bla ani"></div>
+      <i></i>
+      <div class="shadow"></div>
+      <div class="overlay-card"></div>
+    </div>
+
+    <div class="btn-pass">Pass</div>
+
+  </div>
+
+</div>
---
+++ new_public/partials/sub_hand.php
@@ -0,0 +1,14 @@
+<div class="sub-hand">
+
+  <div class="grave">
+    <div class="bla ani"></div>
+    <i class="grave-img"></i>
+    <div class="shadow"></div>
+    <div class="overlay-card"></div>
+  </div>
+
+  <div class="deck-back">
+
+  </div>
+
+</div>
