c38e980375dc5b24931088fc9945d94ac3db12c3
exane <raco0n@gmx.de>
2015-07-03 11:42:13 +0200

    testing monitor tools

--- server/server.js
+++ server/server.js
@@ -1,17 +1,6 @@
 var argv = require('minimist')(process.argv.slice(2));
-/*var SocketCluster = require('socketcluster').SocketCluster;*/
 
-/*
-var socketCluster = new SocketCluster({
-  workers: Number(argv.w) || 1,
-  stores: Number(argv.s) || 1,
-  port: Number(argv.p) || 16918,
-  appName: argv.n || null,
-  workerController: __dirname + '/worker.js',
-  storeController: __dirname + '/store.js',
-  socketChannelLimit: 100,
-  rebootWorkerOnCrash: argv['auto-reboot'] != false
-});*/
+require("monitor").start();
 
 global.connections = require("./Connections")();
 
